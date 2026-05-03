38a725667d3562a80fef1c915c881c07f72a9692
Your Name <you@example.com>
2026-05-03 20:13:12 +0700

    commit

--- OT.sql
+++ OT.sql
@@ -91,3 +91,22 @@ WHERE phone = '0987654321';
 
 DELETE FROM movies 
 WHERE id = 3;
+
+SELECT * 
+FROM movies 
+WHERE duration_minutes BETWEEN 90 AND 120;
+
+SELECT * 
+FROM bookings 
+WHERE showtime_id = 2 
+ORDER BY booking_date DESC;
+
+SELECT * 
+FROM movies 
+WHERE age_restriction = 18 OR duration_minutes > 150;
+
+SELECT * 
+FROM showtimes 
+WHERE ticket_price > 100000 
+  AND MONTH(show_time) = MONTH(CURRENT_DATE())
+  AND YEAR(show_time) = YEAR(CURRENT_DATE());
